aa513e14e811959d4cd490c13da73b112e0c5ea2
Marcus Rådell <marcus@radell.net>
2024-11-07 09:00:00 +0100

    minimal working example with drizzle

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,6 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { pgTable, varchar, integer } from "drizzle-orm/pg-core";
 
 export const pancakesTable = pgTable("pancakes", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  startTimestamp: integer().notNull(),
   status: varchar({ length: 255 }).notNull(),
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import { createApiRouter } from "./api-router";
 import { createHttpApp } from "./app";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { pancakesTable } from "./db/schema";
 
 createHttpApp(createApiRouter()).listen(3000, () => {
   console.log(`Server started on http://localhost:3000.`);
 });
+
+const db = drizzle(process.env.DATABASE_URL!);
+
+async function main() {
+  const pancake: typeof pancakesTable.$inferInsert = {
+    startTimestamp: new Date(),
+    status: "COOKING",
+  };
+
+  await db.insert(pancakesTable).values(pancake);
+  console.log("New pancake created!");
+
+  const users = await db.select().from(pancakesTable);
+  console.log("Getting all pancakes from the database: ", users);
+}
+
+main();
